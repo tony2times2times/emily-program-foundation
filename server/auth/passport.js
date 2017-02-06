4104acceb10aff3e8e9199040e8c7bb7b324cc4c
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2017-02-06 14:57:52 -0600

    Testing

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -14,7 +14,7 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth2').Strategy;
 /** ---------- REQUIRE CUSTOM APP MODULES ---------- **/
-var config = require('../config/auth');
+var config = require('../../../secretSquirrel/secret');
 
 // all db queries moved to a service layer, necessary for proper unit testing
 var UserService = require('../services/user');
