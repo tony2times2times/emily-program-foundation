58dc13844057798b21b828d2f82786252ebe85f7
Amanda Jopp <aclaire488@gmail.com>
2017-02-07 13:58:20 -0600

    all previous and next buttons are working

--- Volunteer/public/scripts/formClient.js
+++ Volunteer/public/scripts/formClient.js
@@ -33,33 +33,65 @@ myForm.config(['$routeProvider', function($routeProvider){
   });
 }]); //end routeProvider
 
-myForm.controller('EssayController', ['$scope', '$http', function($scope, $http){
-
+myForm.controller('EssayController', ['$scope', '$http', '$window', '$location', function($scope, $http, $location, $window){
+  //previous button
+  $scope.essayPrev = function(){
+    window.location ='form#!/volInfo';
+  };
+  //next button
+  $scope.essayNext = function(){
+    window.location = 'form#!/interestSkills';
+  };
 }]); //end EssayController
 
 myForm.controller('FormController', ['$scope', '$http', function($scope, $http){
   console.log('ng sourced');
 }]); //end FormController
 
-myForm.controller('ReferencesController', ['$scope', '$http', function($scope, $http){
-
+myForm.controller('ReferencesController', ['$scope', '$http', '$location', '$window', function($scope, $http, $location, $window){
+  //previous button
+  $scope.refPrev = function(){
+    window.location ='form#!/interestSkills';
+  };
+  //next button
+  $scope.refNext = function(){
+    window.location ='form#!/waiver';
+  };
 }]);
 
 myForm.controller('ReqsController',['$scope', '$http','$location', '$window', function($scope, $http, $location, $window){
+    //begin application button
     $scope.beginApp = function(){
-    $window.location ='form#!/volInfo';
+    window.location ='form#!/volInfo';
   };
 }]);//end ReqsController
 
-myForm.controller('SkillsController', ['$scope', '$http', function($scope, $http){
-
+myForm.controller('SkillsController', ['$scope', '$http', '$location', '$window', function($scope, $http, $location, $window){
+  //previous button
+  $scope.skillsPrev = function(){
+    window.location ='form#!/essayQues';
+  };
+  //next button
+  $scope.skillsNext = function(){
+    window.location ='form#!/references';
+  };
 }]);
 
-myForm.controller('VolInfoController', ['$scope', '$http', function($scope, $http){
-
+myForm.controller('VolInfoController', ['$scope', '$http', '$location', '$window', function($scope, $http, $location, $window){
+  //next button function
+  $scope.infoPrev = function(){
+    window.location = 'form#!/volReqs';
+  };
+  //previous button function
+  $scope.infoNext = function(){
+    window.location = 'form#!/essayQues';
+  };
 }]);//endVolInfoController
 
 
 myForm.controller('WaiverController', ['$scope', '$http', function($scope, $http){
-
+  //previous button
+  $scope.wavPrev = function(){
+    window.location ='form#!/references';
+  };
 }]);
