bf9d85ae8e06bd35d2dfd60941a83e7376c287b8
tony2times2times <tony.peraza86@gmail.com>
2017-02-08 15:51:46 -0600

    displaying active info

--- Admin/public/scripts/controllers/applicantsController.js
+++ Admin/public/scripts/controllers/applicantsController.js
@@ -2,25 +2,256 @@ emilyApp.controller('ApplicantsController', ["$scope", "$http","$timeout", "Volu
 function($scope, $http, $timeout, VolunteerFactory) {
   console.log("ApplicantsController loaded.");
   var init=0;
+  $scope.activeView = 'skills';
   $scope.bucketList= ['APPLIED','PENDING','SCHEDULED','APPROVED','PROGRAM ERROR!!! CHECK bucketList'];
   $scope.hatchery = [];
   $scope.applied = [
-    { 'name': 'Patrick Stewart', 'drag': true },
-    { 'name': 'Nathan Fillion', 'drag': true },
-    { 'name': 'William Shatner', 'drag': true }
+    {
+      'name' :{
+        'first_name' : 'Patrick',
+        'last_name' : 'Stewart',
+      },
+      'address': {
+        'street' :'1234',
+        'city' : 'Patty Lane Drive Road',
+        'state' : 'MN',
+        'zip' : '55736',
+      },
+      'email' : 'SirPatrickStewart@gmail.com',
+      'phoneNum' : '808-867-5309',
+      'dateOfBirth' : '13 July 1940',
+      'emergencyContact' : {
+        'name' : 'Tony Peraza',
+        'phone' : '763-439-3173',
+      },
+      'employment' : 'U.S.S. Enterprise (NCC-1701-D) Captain',
+      'essayOne' : 'Today you are you! That is truer than true! There is no one alive who is you-er than you!',
+      'esssayTwo' : 'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.',
+      'essayThree' : "The more that you read, the more things you will know. The more that you learn, the more places you'll go.",
+      'interests' : 'Fundraising',
+      referenceOne : {
+      'name' : 'Jonathan Frakes',
+      'email' : 'Jonathan.Frankes@gmail.com',
+      'phone' : '952-867-5309',
+    },
+    referenceTwo :{
+      'name' : 'Gates McFadden',
+      'email' : 'Gates.McFadden@gmail.com',
+      'phone' : '952-867-5309',
+    },
+      'skills' : ['blogging', 'public speaking', '3-D chess']
+    },
+
+    {
+      'name' : {
+        'first_name' : 'Nathan',
+        'last_name' : 'Fillion',
+      },
+      'address': {
+        'street' :'1234',
+        'city' : 'Patty Lane Drive Road',
+        'state' : 'MN',
+        'zip' : '55736',
+      },
+      'email' : 'SirPatrickStewart@gmail.com',
+      'dateOfBirth' : '13 July 1940',
+      'emergencyContact' : {
+        'name' : 'Tony Peraza',
+        'phone' : '763-439-3173',
+      },
+      'employment' : 'U.S.S. Enterprise (NCC-1701-D) Captain',
+      'essayOne' : 'Today you are you! That is truer than true! There is no one alive who is you-er than you!',
+      'esssayTwo' : 'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.',
+      'essayThree' : "The more that you read, the more things you will know. The more that you learn, the more places you'll go.",
+      'interests' : 'Fundraising',
+      referenceOne : {
+      'name' : 'Jonathan Frakes',
+      'email' : 'Jonathan.Frankes@gmail.com',
+      'phone' : '952-867-5309',
+    },
+    referenceTwo :{
+      'name' : 'Gates McFadden',
+      'email' : 'Gates.McFadden@gmail.com',
+      'phone' : '952-867-5309',
+    },
+      'skills' : ['blogging', 'public speaking', '3-D chess']
+    },
+
+    {
+      'name' :{
+        'first_name' : 'William',
+        'last_name' : 'Shatner',
+      },
+      'address': {
+        'street' :'1234',
+        'city' : 'Patty Lane Drive Road',
+        'state' : 'MN',
+        'zip' : '55736',
+      },
+      'email' : 'SirPatrickStewart@gmail.com',
+      'dateOfBirth' : '13 July 1940',
+      'emergencyContact' : {
+        'name' : 'Tony Peraza',
+        'phone' : '763-439-3173',
+      },
+      'employment' : 'U.S.S. Enterprise (NCC-1701-D) Captain',
+      'essayOne' : 'Today you are you! That is truer than true! There is no one alive who is you-er than you!',
+      'esssayTwo' : 'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.',
+      'essayThree' : "The more that you read, the more things you will know. The more that you learn, the more places you'll go.",
+      'interests' : 'Fundraising',
+      referenceOne : {
+      'name' : 'Jonathan Frakes',
+      'email' : 'Jonathan.Frankes@gmail.com',
+      'phone' : '952-867-5309',
+    },
+    referenceTwo :{
+      'name' : 'Gates McFadden',
+      'email' : 'Gates.McFadden@gmail.com',
+      'phone' : '952-867-5309',
+    },
+      'skills' : ['blogging', 'public speaking', '3-D chess']
+    },
   ];
   $scope.pending = [
-    { 'name': 'Kate Mulgrew', 'drag': true }
+    {
+      'name' :{
+        'first_name' : 'Kate',
+        'last_name' : 'Mulgrew',
+      },
+      'address': {
+        'street' :'1234',
+        'city' : 'Patty Lane Drive Road',
+        'state' : 'MN',
+        'zip' : '55736',
+      },
+      'email' : 'SirPatrickStewart@gmail.com',
+      'dateOfBirth' : '13 July 1940',
+      'emergencyContact' : {
+        'name' : 'Tony Peraza',
+        'phone' : '763-439-3173',
+      },
+      'employment' : 'U.S.S. Enterprise (NCC-1701-D) Captain',
+      'essayOne' : 'Today you are you! That is truer than true! There is no one alive who is you-er than you!',
+      'esssayTwo' : 'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.',
+      'essayThree' : "The more that you read, the more things you will know. The more that you learn, the more places you'll go.",
+      'interests' : 'Fundraising',
+      referenceOne : {
+      'name' : 'Jonathan Frakes',
+      'email' : 'Jonathan.Frankes@gmail.com',
+      'phone' : '952-867-5309',
+    },
+    referenceTwo :{
+      'name' : 'Gates McFadden',
+      'email' : 'Gates.McFadden@gmail.com',
+      'phone' : '952-867-5309',
+    },
+      'skills' : ['blogging', 'public speaking', '3-D chess']
+    },
   ];
   $scope.scheduled = [
-    { 'name': 'Scott Bakula',
-    'drag': true,
-    onDrop: 'dropCallback'
+    {
+      'name' :{
+        'first_name' : 'Kate',
+        'last_name' : 'Mulgrew',
+      },
+      'address': {
+        'street' :'1234',
+        'city' : 'Patty Lane Drive Road',
+        'state' : 'MN',
+        'zip' : '55736',
+      },
+      'email' : 'SirPatrickStewart@gmail.com',
+      'dateOfBirth' : '13 July 1940',
+      'emergencyContact' : {
+        'name' : 'Tony Peraza',
+        'phone' : '763-439-3173',
+      },
+      'employment' : 'U.S.S. Enterprise (NCC-1701-D) Captain',
+      'essayOne' : 'Today you are you! That is truer than true! There is no one alive who is you-er than you!',
+      'esssayTwo' : 'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.',
+      'essayThree' : "The more that you read, the more things you will know. The more that you learn, the more places you'll go.",
+      'interests' : 'Fundraising',
+      referenceOne : {
+      'name' : 'Jonathan Frakes',
+      'email' : 'Jonathan.Frankes@gmail.com',
+      'phone' : '952-867-5309',
+    },
+    referenceTwo :{
+      'name' : 'Gates McFadden',
+      'email' : 'Gates.McFadden@gmail.com',
+      'phone' : '952-867-5309',
+    },
+      'skills' : ['blogging', 'public speaking', '3-D chess']
+    },
+  ];
+  $scope.approved = [
+    {'name' :{
+      'first_name' : 'Avery',
+      'last_name' : 'Brooks',
+    },
+    'address': {
+      'street' :'1234',
+      'city' : 'Patty Lane Drive Road',
+      'state' : 'MN',
+      'zip' : '55736',
+    },
+    'email' : 'SirPatrickStewart@gmail.com',
+    'dateOfBirth' : '13 July 1940',
+    'emergencyContact' : {
+      'name' : 'Tony Peraza',
+      'phone' : '763-439-3173',
+    },
+    'employment' : 'U.S.S. Enterprise (NCC-1701-D) Captain',
+    'essayOne' : 'Today you are you! That is truer than true! There is no one alive who is you-er than you!',
+    'esssayTwo' : 'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.',
+    'essayThree' : "The more that you read, the more things you will know. The more that you learn, the more places you'll go.",
+    'interests' : 'Fundraising',
+    referenceOne : {
+    'name' : 'Jonathan Frakes',
+    'email' : 'Jonathan.Frankes@gmail.com',
+    'phone' : '952-867-5309',
   },
-];
-$scope.approved = [
-  { 'name': 'Avery Brooks', 'drag': true },
-  { 'name': '	Harrison Ford', 'drag': true }
+  referenceTwo :{
+    'name' : 'Gates McFadden',
+    'email' : 'Gates.McFadden@gmail.com',
+    'phone' : '952-867-5309',
+  },
+    'skills' : ['blogging', 'public speaking', '3-D chess']
+  },
+  {
+    'name' :{
+      'first_name' : 'Harrison',
+      'last_name' : 'Ford',
+    },
+    'address': {
+      'street' :'1234',
+      'city' : 'Patty Lane Drive Road',
+      'state' : 'MN',
+      'zip' : '55736',
+    },
+    'email' : 'SirPatrickStewart@gmail.com',
+    'dateOfBirth' : '13 July 1940',
+    'emergencyContact' : {
+      'name' : 'Tony Peraza',
+      'phone' : '763-439-3173',
+    },
+    'employment' : 'U.S.S. Enterprise (NCC-1701-D) Captain',
+    'essayOne' : 'Today you are you! That is truer than true! There is no one alive who is you-er than you!',
+    'esssayTwo' : 'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.',
+    'essayThree' : "The more that you read, the more things you will know. The more that you learn, the more places you'll go.",
+    'interests' : 'Fundraising',
+    referenceOne : {
+    'name' : 'Jonathan Frakes',
+    'email' : 'Jonathan.Frankes@gmail.com',
+    'phone' : '952-867-5309',
+  },
+  referenceTwo :{
+    'name' : 'Gates McFadden',
+    'email' : 'Gates.McFadden@gmail.com',
+    'phone' : '952-867-5309',
+  },
+    'skills' : ['blogging', 'public speaking', '3-D chess']
+  },
 ];
 $scope.hatchery.push($scope.applied);
 $scope.hatchery.push($scope.pending);
@@ -52,6 +283,17 @@ $scope.incubator = function(index){
   else {
     console.log(indext + " is not a recognized index please check the incubator function");
   }
+};
+
+//sets active Applicant
+$scope.setActive = function(listIndex, personIndex){
+  $scope.person = $scope.hatchery[listIndex][personIndex];
+  $scope.skills = $scope.person.skills;
+  console.log('the active person is: ' + $scope.person.name.first_name, $scope.person.name.last_name);
+};
+$scope.changeView = function (attribute) {
+  console.log('changing view to: ' + attribute);
+    $scope.activeView = attribute;
 };
 
 }]);
