822c2b60621b9ee6bdd32f215668d902cda2d832
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2017-02-10 23:47:31 -0600

    Skills and Interests editor complete.

--- Admin/public/scripts/client.js
+++ Admin/public/scripts/client.js
@@ -1,6 +1,6 @@
 console.log("JS");
 
-var emilyApp = angular.module('emilyApp', ["ngRoute", "ngDragDrop"]);
+var emilyApp = angular.module('emilyApp', ["ngRoute", "ngDragDrop", "ui.toggle"]);
 
 //handle angular routing within the application
 emilyApp.config(["$routeProvider", function($routeProvider) {
