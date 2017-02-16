87a88a3df3a8022b4daaa72326a3533f8ea09492
tony2times2times <tony.peraza86@gmail.com>
2017-02-16 13:21:58 -0600

    completed work on modifying skills and interests

--- Admin/public/scripts/controllers/applicantsController.js
+++ Admin/public/scripts/controllers/applicantsController.js
@@ -1,6 +1,7 @@
 emilyApp.controller('ApplicantsController', ["$scope", "$http","$timeout", "VolunteerFactory",
 function($scope, $http, $timeout, VolunteerFactory) {
-  console.log("ApplicantsController loaded.");
+
+  //global variables
   var init=0;
   $scope.activeView = 'info';
   $scope.selected = {};
@@ -15,15 +16,17 @@ function($scope, $http, $timeout, VolunteerFactory) {
   $scope.hatchery.push($scope.applied);
   $scope.hatchery.push($scope.pending);
   $scope.hatchery.push($scope.scheduled);
+  $scope.skills = [];
+  $scope.interests = [];
 
+  //on page load get all aplicants
   $scope.loadApplicants = function(){
-    //on page load get all aplicants
     $http({
       method: 'GET',
       url: '/applicant',
     }).then(function successCallback(response) {
       console.log(response);
-      //for every volunteer in the response
+      //put each applicant in thier appropriate buck
       for (var i = 0; i < response.data.length; i++) {
         if (response.data[i].appStatus === 'applied') {
           $scope.applied.push(response.data[i]);
@@ -40,6 +43,7 @@ function($scope, $http, $timeout, VolunteerFactory) {
     });
   };
 
+  //get active skills and interests from the database
   $scope.getFormFields = function(){
     $http({
       method: 'GET',
@@ -47,6 +51,7 @@ function($scope, $http, $timeout, VolunteerFactory) {
       data: {status: $scope.person.appStatus}
     }).then(function successCallback(response) {
       console.log(response);
+      //assign the skills and interests to global variables
       $scope.interests = response.data.interests;
       $scope.skills = response.data.skills;
     }, function errorCallback(error) {
@@ -112,7 +117,7 @@ function($scope, $http, $timeout, VolunteerFactory) {
         }
       }
     }
-    //update all the database
+    //updates the database
     $http({
       method: 'PATCH',
       url: '/applicant/missed/' + $scope.person._id,
@@ -126,12 +131,45 @@ function($scope, $http, $timeout, VolunteerFactory) {
 
   //sets active Applicant
   $scope.setActive = function(listIndex, personIndex){
+    var i;
+    var j;
     //create an acite person cloned from the selected person
     $scope.person = angular.copy($scope.hatchery[listIndex][personIndex]);
+    //init the availibleSkills and availibleInterests as an array
+    $scope.person.availibleSkills = [];
+    $scope.person.availibleInterests = [];
+    //check each active skill
+    skill:
+    for (i = 0; i < $scope.skills.length; i++) {
+      //check each skill a applicant has
+      for (j = 0; j < $scope.person.skills.length; j++) {
+        if ($scope.skills[i].skill === $scope.person.skills[j]) {
+          //if the applicant already has the skill continue to the nex skill
+          continue skill;
+        }
+      }
+      //if the skill was not found then add it to the availibleSkills
+      $scope.person.availibleSkills.push($scope.skills[i].skill);
+    }
+    interest:
+    for (i = 0; i < $scope.interests.length; i++) {
+      //check each interest a applicant has
+      for (j = 0; j < $scope.person.interests.length; j++) {
+        if ($scope.interests[i].interest === $scope.person.interests[j]) {
+          //if the applicant already has the interest continue to the nex interest
+          continue interest;
+        }
+      }
+      //if the interest was not found then add it to the availibleInterests
+      $scope.person.availibleInterests.push($scope.interests[i].interest);
+    }
     //create a active person backup
     $scope.savePerson = angular.copy($scope.hatchery[listIndex][personIndex]);
-    console.log('the active person is: ' + $scope.person.name.first_name, $scope.person.name.last_name);
+    console.log('the active person is: ' + $scope.person.name.first_name,
+    $scope.person.name.last_name);
   };
+
+  //Changes info view
   $scope.changeView = function (attribute) {
     console.log('changing view to: ' + attribute);
     $scope.activeView = attribute;
@@ -232,7 +270,7 @@ function($scope, $http, $timeout, VolunteerFactory) {
         if ($scope.hatchery[i][j]._id===$scope.id){
           //update the person in that bucket
           $scope.hatchery[i][j] = angular.copy($scope.person);
-          //exit the bucket for loop
+          //exit the bucket loop
           break bucket;
         }
       }
@@ -256,13 +294,55 @@ function($scope, $http, $timeout, VolunteerFactory) {
     $scope.person = angular.copy($scope.savePerson) ;
   };
 
-  //removes skill from active user
+  //add skill to active applicant user based on the selected skill
+  $scope.addSkill = function(skill){
+    //prevent blank skill from being added.
+    if (typeof skill !== 'string') {
+      return;
+    }
+    //add the skill to the applicants skills list
+    $scope.person.skills.push(skill);
+    //search the applicants availibleSkills
+    for (var i = 0; i < $scope.person.availibleSkills.length; i++) {
+      //when the skill is found remove it from availibleSkills
+      if ($scope.person.availibleSkills[i] === skill) {
+        $scope.person.availibleSkills.splice(i, 1);
+        break;
+      }
+    }
+  };
+
+  //removes skill from active applicant
   $scope.removeSkill = function(index){
+    //add skill to list availibleSkills
+    $scope.person.availibleSkills.push($scope.person.skills[index]);
+    //remove skill from applicant
     $scope.person.skills.splice(index,1);
   };
 
+  //add interst based on the selected interest
+  $scope.addInterest = function(interest){
+    //prevent blank interests from being selected
+    if (typeof interest !== 'string') {
+      return;
+    }
+    //adds interest to applicants interests list
+    $scope.person.interests.push(interest);
+    //search the applicants availibleInterests
+    for (var i = 0; i < $scope.person.availibleInterests.length; i++) {
+      //find the interest and remove it from availibleInterests
+      if ($scope.person.availibleInterests[i] === interest) {
+        $scope.person.availibleInterests.splice(i, 1);
+        break;
+      }
+    }
+  };
+
   $scope.removeInterest = function(index){
-    $scope.person.interests.splice(index,1);
+    //add interest to the applicants availibleInterests
+    $scope.person.availibleInterests.push($scope.person.interests[index]);
+    //remove interest from applicants interests
+    $scope.person.interests.splice(index, 1);
   };
 
   //determines button color based on number of missed orientations
@@ -325,16 +405,6 @@ function($scope, $http, $timeout, VolunteerFactory) {
       }, function errorCallback(error) {
         console.log('error', error);
       });
-    };
-
-    //add skill based on the selected skill
-    $scope.addSkill = function(skill){
-      $scope.person.skills.push(skill);
-    };
-
-    //add interst based on the selected interest
-    $scope.addInterest = function(interest){
-      $scope.person.interests.push(interest);
     };
 
     $scope.getFormFields();
